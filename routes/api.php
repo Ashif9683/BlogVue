3382a12fbb35e9b3420cde94dbf14ff97591faef
Ashif9683 <ashif111200@gmail.com>
2026-03-01 11:23:39 +0530

    added users and posts components

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function (): void {
+    Route::get('users', [UserController::class, 'index'])->name('users.index');
     Route::apiResource('posts', PostController::class);
 });
